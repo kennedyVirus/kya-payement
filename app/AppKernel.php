60cd5fb864677683e41d44646ba5f4b2d195004b
kennedyVirus <44835221+kennedyVirus@users.noreply.github.com>
2020-09-28 20:43:38 +0000

    resolve kernel bug

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,13 +31,13 @@ class AppKernel extends Kernel
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
         }
-        $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-        $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+//        $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+//        $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+//
+//        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+//            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+//            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+//        }
 
         return $bundles;
     }
