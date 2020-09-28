28fe6b2cb06ab4f2404d7d3b924ef34e6d4b1a9c
kennedyVirus <44835221+kennedyVirus@users.noreply.github.com>
2020-09-28 21:29:55 +0000

    ok

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,16 +21,20 @@ class AppKernel extends Kernel
             new SysSecurityBundle\SysSecurityBundle(),
         ];
 
-       // if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 
-         //   if ('dev' === $this->getEnvironment()) {
+            if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-         //   }
-     //   }
+            }
+        }else{
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+        }
 //        $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
 //        $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
 //
