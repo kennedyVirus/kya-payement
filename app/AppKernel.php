eea7665c7b8441e69f650fa526008e7a8a1d18ea
kennedyVirus <44835221+kennedyVirus@users.noreply.github.com>
2020-09-28 21:31:18 +0000

    ok

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -34,6 +34,8 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+
         }
 //        $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
 //        $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
