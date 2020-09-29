ffc6faaca2f008d6dcef3be2c5725d8c2ac560de
kennedyVirus <44835221+kennedyVirus@users.noreply.github.com>
2020-09-29 11:02:26 +0000

    test zedeka

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,5 +19,18 @@ class DefaultController extends Controller
 //        return $this->render('default/index.html.twig', [
 //            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
 //        ]);
+    }
+
+    /**
+     * @Route("/testing/zedeka")
+     */
+
+    public function testingZed(){
+        $URL="https://api.smszedekaa.com/api/v2/SendSMS?ApiKey=ISnqx7tbigE7OQxnGnsBuY4xrZC3m2Uj7wRpbOuIjtk=&ClientId=54911dcd-e69c-4030-9328-4b848c64c4db&SenderId=KYA&Message=HELLO&MobileNumbers=22893643212";
+        $guzzleClient = new Client();
+        $response = $guzzleClient->request('GET', $URL);
+
+
+        return 0;
     }
 }
